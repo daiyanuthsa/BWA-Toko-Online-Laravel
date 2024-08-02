06482a70e91231c8390c1872ef6fb2d6b08d6c0c
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2024-08-03 02:44:02 +0700

    feat: location API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\API\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('register/check', [RegisteredUserController::class,'check'])->name('api-register-check');
+Route::get('provinces', [LocationController::class, 'provinces'])->name('api-provinces');
+Route::get('regencies/{province_id}', [LocationController::class, 'regencies'])->name('regencies');
